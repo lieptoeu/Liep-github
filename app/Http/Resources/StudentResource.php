24f8ecd99406fb98ee879774e48a3be71b732da8
LIEP.TOEUR <LIEP.TOEUR@PNC.LAN>
2024-05-30 07:48:38 +0700

    push laravel

---
+++ app/Http/Resources/StudentResource.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class StudentResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            
+            'id' => $this->id,
+            'name' => $this->name,
+            'age' => $this->age,
+            'province' => $this->province,
+            'score' => $this->score,
+            // 'score' => $this->score >= 50 ? 'pass' : 'fail',
+            'phone_number' => $this->phone_number,
+            'status' => $this->status
+        ];
+    }
+}
